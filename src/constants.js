b212fae7f555e11e0a0b259b875f86ecd5ac4e93
fakhergh <fekhergh@xtech.guru>
2021-01-29 14:50:44 +0100

    TD-01 Define constants

---
+++ src/constants.js
@@ -0,0 +1,4 @@
+export const GRAPHQL_END_POINT =
+  "https://phbngzrpsbap5agiy6qxizbfam.appsync-api.eu-central-1.amazonaws.com/graphql";
+
+export const GRAPHQL_API_KEY = "da2-3wg44mzhgzax5dyydmgatrmyhi";
