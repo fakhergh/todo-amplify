1a8c21ffe71789543909125ecca846d6d51ca3fb
fakhergh <fekhergh@xtech.guru>
2021-02-01 20:44:23 +0100

    TD-02 Use Formik for SignIn and SignUp

--- src/pages/SignIn.js
+++ src/pages/SignIn.js
@@ -1,13 +1,44 @@
 import React from "react";
 import { SignIn as BaseSignIn } from "aws-amplify-react";
+import { Auth } from "aws-amplify";
+import { LoginForm } from "../views";
+
+const styles = {
+  signUpButton: {
+    backgroundColor: "transparent",
+    borderWidth: 0,
+  },
+};
 
 class SignIn extends BaseSignIn {
+  onSubmit = (values) => {
+    Auth.signIn(values).catch((error) => {
+      alert("Sign In Error");
+      console.log("SignIn error: ", error);
+    });
+  };
+
+  showSignUp = () => {
+    this.props.onStateChange("signUp");
+  };
+
   render() {
-    if (this.props.authState === "signIn") {
-      return <div>sign in</div>;
+    if (this.props.authState !== "signIn") {
+      return null;
     }
 
-    return null;
+    return (
+      <div>
+        <LoginForm onSubmit={this.onSubmit} />
+        <button
+          style={styles.signUpButton}
+          type="button"
+          onClick={this.showSignUp}
+        >
+          Don't have account? Sign up
+        </button>
+      </div>
+    );
   }
 }
 
--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -1,13 +1,59 @@
 import React from "react";
 import { SignUp as BaseSignUp } from "aws-amplify-react";
+import { Auth } from "aws-amplify";
+
+import { RegisterForm } from "../views";
+
+const styles = {
+  signUpButton: {
+    backgroundColor: "transparent",
+    borderWidth: 0,
+  },
+};
 
 class SignUp extends BaseSignUp {
+  onSubmit = (values) => {
+    const data = {
+      username: values.username,
+      password: values.password,
+      attributes: {
+        email: values.email,
+        phone_number: values.phoneNumber,
+      },
+    };
+
+    Auth.signUp(data)
+      .then((signUpData) => {
+        console.log("signUpData: ", signUpData);
+      })
+      .catch((error) => {
+        alert("Sign Up Error");
+        console.log("SignIn error: ", error);
+      });
+  };
+
+  showSignIn = () => {
+    this.props.onStateChange("signIn");
+  };
+
   render() {
-    if (this.props.authState === "signUp") {
-      return <div>sign up</div>;
+    if (this.props.authState !== "signUp") {
+      return null;
     }
 
-    return null;
+    return (
+      <div>
+        <RegisterForm onSubmit={this.onSubmit} />
+
+        <button
+          style={styles.signUpButton}
+          type="button"
+          onClick={this.showSignIn}
+        >
+          Back to login
+        </button>
+      </div>
+    );
   }
 }
 
