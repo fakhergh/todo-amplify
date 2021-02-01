3bc78d32cc1621c49a178e19331344fb6b0ccf03
fakhergh <fekhergh@xtech.guru>
2021-02-01 20:44:38 +0100

    TD-02 Create hsitory instance

--- src/history.js
+++ src/history.js
@@ -1,3 +1,20 @@
 import { createBrowserHistory } from "history";
+import { Auth } from "aws-amplify";
 
-export default createBrowserHistory();
+const history = createBrowserHistory();
+
+history.listen((location) => {
+  Auth.currentSession()
+    .then(() => {
+      if (location.pathname === "/auth") {
+        history.push("/");
+      }
+    })
+    .catch(() => {
+      if (location.pathname !== "/auth") {
+        history.push("/auth");
+      }
+    }).catch(console.log);
+});
+
+export default history;
