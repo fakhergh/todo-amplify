1ea1f3a0c802a0f0722a522f3aee98fe290307bf
fakhergh <fekhergh@xtech.guru>
2021-02-01 20:43:57 +0100

    TD-02 Update Auth page

--- src/pages/Auth.js
+++ src/pages/Auth.js
@@ -1,34 +1,26 @@
 import React, { useCallback } from "react";
-import { Authenticator, ConfirmSignUp } from "aws-amplify-react";
+import { Authenticator } from "aws-amplify-react";
 
 import SignIn from "./SignIn";
 import SignUp from "./SignUp";
 
-const AlwaysOn = (props) => {
+const Auth = function ({ history }) {
+  const onStateChange = useCallback(
+    (state) => {
+      if (state === "signedIn") {
+        history.push("/");
+      }
+    },
+    [history]
+  );
+
   return (
     <div>
-      <div>I am always here to show current auth state: {props.authState}</div>
-      <button onClick={() => props.onStateChange("signUp")}>
-        Show Sign Up
-      </button>
+      <Authenticator hideDefault={true} onStateChange={onStateChange}>
+        <SignIn />
+        <SignUp />
+      </Authenticator>
     </div>
-  );
-};
-
-const Auth = function () {
-  const onStateChange = useCallback((state) => {
-    if (state === "signedIn") {
-      /* Do something when the user has signed-in */
-    }
-  }, []);
-
-  return (
-    <Authenticator hideDefault={true} onStateChange={onStateChange}>
-      <SignIn authState="signIn" />
-      <SignUp authState="signUp" />
-      <ConfirmSignUp />
-      <AlwaysOn />
-    </Authenticator>
   );
 };
 
