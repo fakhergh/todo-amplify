41128eda08a1981e27f5f15e46b2461c98c9750c
fakhergh <fekhergh@xtech.guru>
2021-02-02 13:47:24 +0100

    TD-02 Create ResetPasswordForm component

---
+++ src/views/ResetPasswordForm.js
@@ -0,0 +1,41 @@
+import React from "react";
+import { Formik, Form } from "formik";
+import * as Yup from "yup";
+
+import { TextInputField } from "../components";
+
+const initialValues = {
+  code: "",
+  password: "",
+  confirmPassword: "",
+};
+
+const validationSchema = Yup.object().shape({
+  code: Yup.string().required(),
+  password: Yup.string().required(),
+  confirmPassword: Yup.string()
+    .oneOf([Yup.ref("password")], "Password not matches")
+    .required(),
+});
+
+const ResetPasswordForm = function ({ onSubmit }) {
+  return (
+    <Formik
+      initialValues={initialValues}
+      validationSchema={validationSchema}
+      onSubmit={onSubmit}
+    >
+      <Form>
+        <TextInputField name="code" placeholder="Code" />
+        <TextInputField name="password" placeholder="New Password" />
+        <TextInputField
+          name="confirmPassword"
+          placeholder="Confirm New Password"
+        />
+        <button type="submit">Submit</button>
+      </Form>
+    </Formik>
+  );
+};
+
+export default ResetPasswordForm;
